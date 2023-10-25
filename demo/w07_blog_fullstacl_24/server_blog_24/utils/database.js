c5f3ad3180f021ae5dfae503c7848de2051c1742
unknown <209410124@gme.tku.edu.tw>
2023-10-25 22:17:19 +0800

    w07

---
+++ demo/w07_blog_fullstacl_24/server_blog_24/utils/database.js
@@ -0,0 +1,42 @@
+import pkg from 'pg';
+const{Pool}=pkg;
+import dotenv from 'dotenv';
+dotenv.config();
+
+let pool;
+
+if (process.env.DATABASE === 'SUPABASE'){
+    pool = new Pool({
+        host:process.env.SUPABASE_HOST,
+        port:'5432',
+        user:'postgres',
+        password:process.env.SUPABASE_PASSWORD,
+        database:'postgres',
+    });
+
+console.log('supabase');
+}else{
+    pool =new Pool({
+        host:'localhost',
+        port:'5432',
+        user:'postgres',
+        password:'910226',
+        database:'wp1_demo_24',
+    });
+    console.log('local')
+ }
+
+console.log('connect database',pool.options);
+
+// const testDB =async()=>{
+//     try{
+//     const results =  await pool.query(`select * from card_24`);
+//     console.log('json data',JSON.stringify(results.rows));
+//     }catch(error){
+//         console.log(error);
+//     }
+//      };
+// testDB();
+
+// module.exports =pool;
+ export default pool;
