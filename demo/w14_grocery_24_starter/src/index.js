c5c59d18d010a08135d869f71cef214cb31f33d7
unknown <209410124@gme.tku.edu.tw>
2023-12-13 21:26:55 +0800

    w14

---
+++ demo/w14_grocery_24_starter/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App_24 from './App_24';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App_24 />
+  </React.StrictMode>
+);
